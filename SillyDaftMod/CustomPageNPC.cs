80cb26130b68283134b072078e7c18e716af2a7c
ExterminatorX99 <kristijan.kos99@gmail.com>
2022-06-28 23:25:09 +0200

    Add custom shop page example

---
+++ SillyDaftMod/CustomPageNPC.cs
@@ -0,0 +1,62 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace SillyDaftMod;
+
+internal class CustomPageNPC : GlobalNPC
+{
+    public override void SetupShop(int type, Chest shop, ref int nextSlot)
+    {
+        if (type == NPCID.Dryad)
+        {
+            SetupCustomPages(shop, ref nextSlot);
+        }
+    }
+
+    //This method demonstrates how you can add simple custom item pages,
+    //without needing to add a reference to Shop Expander.
+    private void SetupCustomPages(Chest shop, ref int nextSlot)
+    {
+        Item[] items1 = new Item[]
+        {
+            MakeItem(ItemID.DirtBlock, "Cube of Earth"),
+            MakeItem(ItemID.MoneyTrough, "Bar of Soap"),
+            MakeItem(ItemID.CopperShortsword, "Legendary Terrablade"),
+            MakeItem(ItemID.Wood, "Spaghetti")
+        };
+
+        Item[] items2 = new Item[]
+        {
+            MakeItem(ItemID.StoneBlock, "Cube of Rock"),
+            MakeItem(ItemID.LastPrism, "First Prism"),
+            MakeItem(ItemID.EmptyBucket, "Stylish Hat"),
+            MakeItem(ItemID.Shadewood, "Spicy Spaghetti")
+        };
+
+        if (ModLoader.TryGetMod("ShopExpander", out var shopMod))
+        {
+            shopMod.Call("AddPageFromArray", "Cool Items", -2, items1);
+            shopMod.Call("AddPageFromArray", "Even Cooler Items", -1, items2);
+        }
+        else
+        {
+            //If Shop Expander isn't loaded, fall back to vanilla
+            foreach (var item in items1)
+                shop.item[nextSlot++] = item;
+            foreach (var item in items2)
+                shop.item[nextSlot++] = item;
+        }
+    }
+
+    private static Item MakeItem(int type, string name)
+    {
+        Item item = new Item(type);
+        item.SetNameOverride(name);
+        item.rare = ItemRarityID.Blue;
+        if (item.value == 0)
+            item.value = 1;
+        item.value *= 11;
+        return item;
+    }
+}
--- SillyDaftMod/SillyDaftGlobalNpc.cs
+++ SillyDaftMod/SillyDaftGlobalNpc.cs
@@ -6,6 +6,9 @@ namespace SillyDaftMod
 {
     internal class SillyDaftGlobalNpc : GlobalNPC
     {
+        //This method demonstrates how Shop Expander can handle mods, that add
+        //way too many items using the vanilla method.
+        //(To allow this many items, the provision size was changed in SillyDaftMod.PostSetupContent)
         public override void SetupShop(int type, Chest shop, ref int nextSlot)
         {
             if (type == NPCID.Dryad)
